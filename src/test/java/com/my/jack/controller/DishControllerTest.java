5ed2bec6a587c46c3bc1c342f6e7a0edf8f2f572
liujiahao <1265684394@qq.com>
2022-09-19 15:27:32 +0800

    update target

---
+++ src/test/java/com/my/jack/controller/DishControllerTest.java
@@ -0,0 +1,43 @@
+package com.my.jack.controller;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DishControllerTest {
+    @InjectMocks
+    DishController dishController;
+
+    @Test
+    void page() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void stopSale() {
+    }
+
+    @Test
+    void sale() {
+    }
+
+    @Test
+    void list() {
+    }
+}
---
+++ src/test/java/com/my/jack/utils/SendEmailTest.java
@@ -0,0 +1,7 @@
+package com.my.jack.utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SendEmailTest {
+
+}
